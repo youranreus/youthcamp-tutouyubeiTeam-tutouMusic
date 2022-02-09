dc9e2a8ca288c3da83b3b96bdb6540d9120cceb9
季悠然 <youranreus@qq.com>
2022-02-09 14:55:33 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,9 +12,7 @@ import routes from './routes'
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+  const createHistory = createWebHashHistory
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
